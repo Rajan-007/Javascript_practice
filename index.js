fba4a4f68baa6976470c08cbafe7010eb24cf273
Rajan-007 <aravinthrajan034@gmail.com>
2023-04-27 19:03:03 +0530

    operators

---
+++ index.js
@@ -0,0 +1,36 @@
+// operators
+
+// Arithmatic operators
+
+a = 1;
+b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+// assignment operators (shorthand)
+
+c=10;
+c +=3;
+console.log(c)
+c -=3;
+console.log(c)
+c *=3;
+console.log(c)
+
+c /=3;
+console.log(c)
+
+// typeof 
+console.log(typeof c );
+console.log(typeof 'Raj' );
+
+// automatic type coversion
+
+d="5"-1
+console.log(d);
+d = "two"*10;
+console.log(d);
+
+
